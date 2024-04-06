65ce0cea24d479b77e84144080e0ba2a262baef2
moregorenine <moregorenine@gmail.com>
2024-04-06 16:58:45 +0900

    refactoring

---
+++ src/datas/StudyResults.ts
@@ -0,0 +1,103 @@
+export type StudyResult = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export let studyResults: StudyResult[] = [
+  {
+    id: 1,
+    title: "Study Result 1",
+    description: "This is the first study result.",
+  },
+  {
+    id: 2,
+    title: "Study Result 2",
+    description: "This is the second study result.",
+  },
+  {
+    id: 3,
+    title: "Study Result 3",
+    description: "This is the third study result.",
+  },
+  {
+    id: 4,
+    title: "Study Result 4",
+    description: "This is the fourth study result.",
+  },
+  {
+    id: 5,
+    title: "Study Result 5",
+    description: "This is the fifth study result.",
+  },
+  {
+    id: 6,
+    title: "Study Result 6",
+    description: "This is the sixth study result.",
+  },
+  {
+    id: 7,
+    title: "Study Result 7",
+    description: "This is the seventh study result.",
+  },
+  {
+    id: 8,
+    title: "Study Result 8",
+    description: "This is the eighth study result.",
+  },
+  {
+    id: 9,
+    title: "Study Result 9",
+    description: "This is the ninth study result.",
+  },
+  {
+    id: 10,
+    title: "Study Result 10",
+    description: "This is the tenth study result.",
+  },
+  {
+    id: 11,
+    title: "Study Result 11",
+    description: "This is the eleventh study result.",
+  },
+  {
+    id: 12,
+    title: "Study Result 12",
+    description: "This is the twelfth study result.",
+  },
+  {
+    id: 13,
+    title: "Study Result 13",
+    description: "This is the thirteenth study result.",
+  },
+  {
+    id: 14,
+    title: "Study Result 14",
+    description: "This is the fourteenth study result.",
+  },
+  {
+    id: 15,
+    title: "Study Result 15",
+    description: "This is the fifteenth study result.",
+  },
+  {
+    id: 16,
+    title: "Study Result 16",
+    description: "This is the sixteenth study result.",
+  },
+  {
+    id: 17,
+    title: "Study Result 17",
+    description: "This is the seventeenth study result.",
+  },
+  {
+    id: 18,
+    title: "Study Result 18",
+    description: "This is the eighteenth study result.",
+  },
+  {
+    id: 19,
+    title: "Study Result 19",
+    description: "This is the nineteenth study result.",
+  },
+];
